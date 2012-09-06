e9284de4bec2dcdb0a2f3e24a31d5b351c86ee1f
lkastler <lkastler@uni-koblenz.de>
2012-09-06 17:44:47 +0200

    added some stuff, no further details

--- js/conf.js
+++ js/conf.js
@@ -3,5 +3,5 @@ var files = new Array(	//"js/modules/stadtbezirke_muenchen.js",
 						//"js/modules/bezirks_kriterien.js",
 						//"js/modules/test_module.js",
 						//"js/modules/test_module2.js",
-						"js/modules/voronoi.js"
+						//"js/modules/voronoi.js"
 						);
