71d42e72cf03819536bb54416b40a1a2da388d50
lkastler <lkastler@uni-koblenz.de>
2012-09-17 14:07:00 +0200

    stuff

--- js/main__vars.js
+++ js/main__vars.js
@@ -1,11 +1,11 @@
 var features = ['age','cars','households','unemployment','fluctuation'];
 var features_labels = ['Altersdurchschnitt','Verkehrsdichte','Haushaltsstruktur','Erwerbst&auml;tigenquote'];
-var features_selection = [true,true,true,true];
+var features_selection = [false,false,false,false];
 var features_weight = [0,0,0,0];	// weights are in [0,1]
 
 var pois = ['kindergartens','gasstations','schools','supermarkets','restaurants','doctors','pharmacies','hardwarestores'];
 var pois_labels = ['Kinderg&auml;rten','Tankstellen','Schulen','Superm&auml;rkte','Restaurants','&Auml;rzte','Apotheken','Baum&auml;rkte'];
-var pois_selection = [true,true,true,true,true,true,true,true];
+var pois_selection = [false,false,false,false,false,false,false,false];
 var pois_weight = [0,0,0,0,0,0,0,0];	// weights are in [0,1]
 
 poi_setup = {
